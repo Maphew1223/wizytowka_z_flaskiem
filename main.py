784663a887edf1c61798cc8f6d1e82c36a428f77
Mateusz Nawrocki <nawrockim95@gmail.com>
2024-10-09 20:51:51 +0200

    add static templates main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
 
 app = Flask(__name__)
 
@@ -8,10 +10,17 @@ def homepage():
     greetings = "Witaj na mojej stronie"
     return greetings
 
+
 @app.route('/me')
 def pierwsza():
        return render_template("pierwsza.html")
 
-@app.route('/contact', methods=['GET'])
-def druga():
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+   if request.method == 'GET':
+       print("We received GET")
        return render_template("druga.html")
+   elif request.method == 'POST':
+       print("We received POST")
+       print(request.form)
+       return redirect("/contact")
